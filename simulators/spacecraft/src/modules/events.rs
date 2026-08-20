781c1f228ff369cf43ea74f61391fa3ebd6e46d6
ThomasLeMontagner <thomas.le-montagner@hotmail.fr>
2026-08-20 07:04:47 +0200

    structured Event

--- simulators/spacecraft/src/modules/events.rs
+++ simulators/spacecraft/src/modules/events.rs
@@ -1,20 +1,41 @@
 use crate::modules::mode::Mode;
 use serde::Serialize;
+use chrono::{DateTime, Utc};
 
 #[derive(Debug, Serialize)]
+#[serde(rename_all = "snake_case")]
 enum ComponentEvent {
     LowBatteryVoltage,
     HighTemperature,
 }
 
 #[derive(Debug, Serialize)]
+#[serde(rename_all = "snake_case")]
 pub struct ModeChangeEvent {
     from: Mode,
     to: Mode,
 }
 
 #[derive(Debug, Serialize)]
-pub enum Event {
-    ComponentEvent,
-    ModeChangeEvent,
+#[serde(rename_all = "snake_case")]
+pub enum EventType {
+    Component(ComponentEvent),
+    ModeChange(ModeChangeEvent),
+}
+
+/// Severity of an event.
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Severity {
+    Information,
+    Warning,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Event {
+    time: DateTime<Utc>,
+    source: String,
+    event_type: EventType,
+    severity: Severity,
+    message: String,
 }
